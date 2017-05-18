f7b5eb4cb963af14b314bcf550c6204a274d8f52
Daniel Ostrovsky <danielos@compit.com>
2017-05-18 10:04:23 +0300

    local-storage

--- src/app/shared/state-core-module/state-core-module.ts
+++ src/app/shared/state-core-module/state-core-module.ts
@@ -4,19 +4,12 @@ import { EffectsModule } from "@ngrx/effects";
 import { TodosReducer } from "./todos-reducer";
 import { TodoFacade } from "./todo-facade";
 import { StatesEffects } from "./state-effects";
+import { compose } from "@ngrx/core";
 import { localStorageSync } from "ngrx-store-localstorage";
-import { compose } from "@ngrx/core/compose";
 
 export const EAGER_REDUCERS = {
     todosReducer: TodosReducer
 };
-
-// StoreModule.provideStore(
-//     compose(
-//         localStorageSync(['todos']),
-//         combineReducers
-//     )({todos, visibilityFilter})
-// )
 
 export function createReducer(asyncReducers = {}) {
     let allReducers = Object.assign(EAGER_REDUCERS, asyncReducers);
@@ -27,15 +20,14 @@ export function appReducer(state: any, action: any) {
     return createReducer()(state, action);
 }
 
+export function l() {
+    return compose(localStorageSync(['todosReducer'], true), combineReducers)(EAGER_REDUCERS);
+}
+
 @NgModule({
     imports: [
         EffectsModule.run(StatesEffects),
-        StoreModule.provideStore(
-            compose(
-                localStorageSync(['todosReducer']),
-                combineReducers
-            )({TodosReducer})
-        )
+        StoreModule.provideStore(compose(localStorageSync(['todosReducer'], true), combineReducers)(EAGER_REDUCERS))
     ],
     providers: [
         TodoFacade
